f81e872ecc3f01da7194225e25024486720faa7b
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-09-17 17:36:40 +0300

    Step 5

---
+++ index.js
@@ -0,0 +1,12 @@
+// Q1)Rewrite the following function as an arrow function:
+/******************************
+	function greet(name) {
+	  return `Hello, ${name}!`;
+	}
+********************************/
+
+// Q2) Write a simple arrow function that takes two parameters and returns their sum.
+
+// Q3) Write a simple arrow function that squares a number.
+
+// Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
